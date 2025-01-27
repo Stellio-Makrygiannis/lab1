be696d67e80fee6e9645d35ec82be7002a417a60
B1nus <119787571+B1nus@users.noreply.github.com>
2025-01-27 18:13:38 +0100

    Lite fler test

--- test/CarGetters.java
+++ test/CarGetters.java
@@ -7,14 +7,29 @@ import static junit.framework.TestCase.assertEquals;
 
 public class CarGetters {
     private Saab95 saab;
+    private Volvo240 volvo;
 
     @Before
     public void init() {
         saab = new Saab95();
+        volvo = new Volvo240();
     }
 
     @Test
     public void testDoorAmount() {
         assertEquals(saab.getNrDoors(), 2);
+        assertEquals(volvo.getNrDoors(), 4);
+    }
+
+    @Test
+    public void testEnginePower() {
+        assertEquals(saab.getEnginePower(), 125.0);
+        assertEquals(volvo.getEnginePower(), 100.0);
+    }
+
+    @Test
+    public void testColor() {
+        assertEquals(saab.getColor(), Color.red);
+        assertEquals(volvo.getColor(), Color.black);
     }
 }
