3646d619be1dcd58789e60e40dbaedcf3587966c
B1nus <119787571+B1nus@users.noreply.github.com>
2025-02-03 13:49:24 +0100

    Added scania truck

--- test/TestSuite.java
+++ test/TestSuite.java
@@ -1,7 +1,8 @@
+
 import org.junit.runner.RunWith;
 import org.junit.runners.Suite;
 
 @RunWith(Suite.class)
-@Suite.SuiteClasses({TestCarGetters.class, TestGasAndBreak.class})
+@Suite.SuiteClasses({TestCarGetters.class, TestGasAndBreak.class, TestScaniaMethods.class})
 public class TestSuite {
 }
