ec6a07dc9a23c25c739755a5a96ccabebecb1de5
Lini <119787571+B1nus@users.noreply.github.com>
2025-02-12 14:00:09 +0100

    Code rueuse and fix "trög kod" Truck.java

--- src/Truck.java
+++ src/Truck.java
@@ -17,23 +17,32 @@ public class Truck extends Car {
     public double getTruckBedPositionMax() {
         return truckBedPositionMax;
     }
+
+    public boolean isTruckBedRaised() {
+        return getTruckBedPosition() > 0;
+    }
+    
     @Override
-    public void move() {if (truckBedPosition > 0) {
-        throw new IllegalStateException("Cannot move while truck bed is raised!");}
+    public void move() {
+        if (getTruckBedPosition() > 0) {
+            throw new IllegalStateException("Cannot move while truck bed is raised!");
+        }
         super.move();
     }
 
     @Override
     protected void incrementSpeed(double amount) {
-        if (truckBedPosition != 0) {
-            throw new IllegalStateException("Cannot accelerate with raised truck bed.");}
+        if (isTruckBedRaised()) {
+            throw new IllegalStateException("Cannot accelerate with raised truck bed.");
+        }
         super.incrementSpeed(amount);
     }
 
     @Override
     protected void decrementSpeed(double amount) {
-        if (truckBedPosition != 0) {
-            throw new IllegalStateException("Cannot decelerate with raised truck bed.");}
+        if (isTruckBedRaised()) {
+            throw new IllegalStateException("Cannot decelerate with raised truck bed.");
+        }
         super.decrementSpeed(amount);
     }
 
@@ -41,8 +50,8 @@ public class Truck extends Car {
         if (getCurrentSpeed() > 0) {
             throw new IllegalStateException("Cannot change truck bed position while moving.");
         }
-        if (newTruckBedPosition < 0 || newTruckBedPosition > truckBedPositionMax) {
-            throw new IllegalArgumentException(String.format("Degree must be between 0 and %f.", truckBedPositionMax));
+        if (newTruckBedPosition < 0 || newTruckBedPosition > getTruckBedPositionMax()) {
+            throw new IllegalArgumentException(String.format("Degree must be between 0 and %f.", getTruckBedPositionMax()));
         }
         truckBedPosition = newTruckBedPosition;
     }
